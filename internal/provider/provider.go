f70faee4258eba4631d01f132ef1f9fefd014602
Coderon_Lee <1710034886@qq.com>
2026-06-05 13:17:40 +0800

    feat: add cloudtower_vds datasource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -66,6 +66,7 @@ func New(version string) func() *schema.Provider {
 				"cloudtower_vm_snapshot":                 dataSourceVmSnapshot(),
 				"cloudtower_vm_template":                 dataSourceVmTemplate(),
 				"cloudtower_content_library_vm_template": dataSourceContentLibraryVmTemplate(),
+				"cloudtower_vds":                         dataSourceVds(),
 			},
 			ResourcesMap: map[string]*schema.Resource{
 				"cloudtower_datacenter":                  resourceDatacenter(),
